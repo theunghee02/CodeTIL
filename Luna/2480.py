08e48dbc1917e6214e207ffd3d40b6aad2f3902e
Minju Park <80474535+parkminju20211126@users.noreply.github.com>
2023-07-20 00:22:20 +0900

    7/20 주사위 세개

---
+++ Luna/2480.py
@@ -0,0 +1,7 @@
+A,B,C = map(int, input().split())
+
+if(A== B== C) : print(10000 + A*1000)
+elif (A == B): print(1000 + A*100)
+elif (A ==C) : print(1000 + A*100)
+elif (B == C) : print(1000 + B*100)
+else : print(100 * max(A,B,C))
