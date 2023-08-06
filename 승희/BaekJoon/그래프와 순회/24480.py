a18062c34952a55d50828c48245c90ebaf1a84fe
theunghee02 <theunghee02@gmail.com>
2023-08-07 01:07:11 +0900

    8/7 알고리즘 수업 - 깊이 우선 탐색 2

---
+++ 승희/BaekJoon/그래프와 순회/24480.py
@@ -0,0 +1,24 @@
+def DFS(graph, v, visited) :
+    global n
+    visited[v] = n
+    graph[v].sort(reverse=True)
+    for i in graph[v] :
+        if not visited[i] :
+            n += 1
+            DFS(graph, i, visited)
+
+import sys
+sys.setrecursionlimit(150000)
+n = 1
+N,M,R = map(int, sys.stdin.readline().rstrip().split())
+graph = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+
+for i in range(M) :
+    a,b = map(int, sys.stdin.readline().rstrip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+DFS(graph, R, visited)
+for i in range(1, N+1):
+    print(visited[i])
