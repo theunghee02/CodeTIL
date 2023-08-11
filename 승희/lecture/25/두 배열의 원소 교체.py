c870418dbdc121a1032df458e3884734c419120a
theunghee02 <theunghee02@gmail.com>
2023-08-12 03:48:39 +0900

    8/12 [lecture] 두 배열의 원소 교체
    예시 답안과 거의 일치한 풀이였다~ㅎㅎ

---
+++ 승희/lecture/25/두 배열의 원소 교체.py
@@ -0,0 +1,11 @@
+import sys
+N,K = map(int, sys.stdin.readline().rstrip().split())
+A = list(map(int, sys.stdin.readline().rstrip().split()))
+B = list(map(int, sys.stdin.readline().rstrip().split()))
+A.sort()
+B.sort(reverse=True)
+for i in range(K) :
+    if A[i] < B[i] :
+        A[i], B[i] = B[i], A[i]
+
+print(sum(A))
