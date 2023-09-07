545b4079a6af857f862dc147bb932101d0cfb3f5
theunghee02 <theunghee02@gmail.com>
2023-09-08 01:58:33 +0900

    9/8 [SQL] 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기
    GROUP BY와 함께 쓰는 MAX()의 사용법
    ㄴ 묶인 애들 중 '대여중'은 True 값으로 False보다 크기 때문에 MAX()에서 True 값이 나옴
    만약 True가 없는 id면 False로 나와 '대여 가능'이 됨
    BETWEEN을 상기시키자!

---
+++ 승희/Programmers/SQL/GROUP BY/157340.sql
@@ -0,0 +1,5 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/157340
+SELECT CAR_ID, IF(MAX('2022-10-16' between START_DATE AND END_DATE), '대여중', '대여 가능') as AVAILABILITY
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY CAR_ID
+ORDER BY CAR_ID DESC;
