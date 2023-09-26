32f93ddb55ba770b92a47630fa97b4121bbd7429
theunghee02 <theunghee02@gmail.com>
2023-09-27 02:37:45 +0900

    9/27 알고리즘 수업 - 너비 우선 탐색 1
    참고 : https://takeu.tistory.com/242
    코테 정체기..
    이해 못 함

---
+++ 승희/BaekJoon/그래프와 순회/24444.py
@@ -0,0 +1,30 @@
+def bfs(v) :
+    global cnt
+
+    q = deque([R])
+    visited[R] = 1
+    while q :
+        v = q.popleft()
+        graph[v].sort()
+        for i in graph[v] :
+            if visited[i] == 0 :
+                cnt += 1
+                visited[i] = cnt
+                q.append(i)
+
+import sys
+from collections import deque
+N, M, R = map(int, sys.stdin.readline().rstrip().split())
+graph = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+cnt = 1
+
+for _ in range(M) :
+    u,v = map(int, sys.stdin.readline().rstrip().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+bfs(R)
+
+for j in visited[1:] :
+    print(j)
