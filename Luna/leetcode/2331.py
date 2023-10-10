95a70dfa08c53f04b3e40b30708505b254e1b488
mjpark <80474535+parkminju20211126@users.noreply.github.com>
2023-10-11 03:08:37 +0900

    10/10 evaluate boolean binary tree

---
+++ Luna/leetcode/2331.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def evaluateTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        if (root.val == 0) : return False
+        elif root.val == 1 : return True
+        b1 = self.evaluateTree(root.left)
+        b2 = self.evaluateTree(root.right)
+        if root.val == 2: return b1 or b2
+        else :return b1 and b2 
