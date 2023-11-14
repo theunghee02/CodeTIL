bc68536e9fbbfdea48fe2a8fa62adebbd47d152f
mjpark <80474535+parkminju20211126@users.noreply.github.com>
2023-11-14 14:19:09 +0900

    11/14 2solved

---
+++ Luna/1541.py
@@ -0,0 +1,19 @@
+## - 오기 전까지 쪼기
+## 각각 계산
+## 뺄셈
+arr = input().split('-')
+## 최종적으로 뺄셈을 하기 위한 저장할 곳
+numbers = []
+for i in arr:
+    sum = 0
+    ## 덧셈을 하기 위해 split -> 덧셈 계산
+    tmp = i.split('+')
+    for j in tmp:
+        sum += int(j)
+    numbers.append(sum)
+print(numbers)
+sum2 = numbers[0]
+for i in range (1,len(numbers)):
+    sum2 -= numbers[i]
+    ##sum2 = sum2 - numbers[1]
+print(sum2)
--- Luna/1904.py
+++ Luna/1904.py
@@ -1,7 +1,11 @@
 def sol(n):
     if n == 1: return 1
     if n == 2: return 2
-    dp[n] = sol(n-1) + sol(n-2)
+    # 이미 계산된 적이 있다면
+    if dp[n] != 0:
+        return dp[n]
+    ## 계산되지 않았다면, 저장
+    dp[n] = (sol(n-1) + sol(n-2)) %15746
     return dp[n]
 
 N = int(input())
