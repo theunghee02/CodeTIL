d5efae36f0e1bbefd947e8d9aadabc0e0f4a51b7
mjpark <80474535+parkminju20211126@users.noreply.github.com>
2023-11-26 19:04:11 +0900

    11/25 16928

--- Luna/16928.py
+++ Luna/16928.py
@@ -1,19 +1,37 @@
 from collections import deque
+
+def bfs(start, target):
+    queue = deque()
+    queue.append([start, 0])  # 1번칸부터 시작
+    
+    while len(queue) != 0:
+        now, cnt = queue.popleft()
+        if now == target:
+            return cnt
+        if visited[now] == 0:
+            visited[now] = 1
+            for i in range(1, 7):
+                # 현재 위치에서 주사위의 값 더함
+                check = i + now
+                if check in ladder.keys():
+                    check = ladder[check]
+                if check in snake.keys():
+                    check = snake[check]
+                if check <= 100 and visited[check] == 0:
+                    queue.append([check, cnt + 1])
+
 ladder_num, snake_num = map(int, input().split())
-matrix = [0] * 101
-visited = [False] * 101
+visited = [0] * 101  # 입력으로 받는 숫자 중 가장 큰 값에 따라 크기 동적으로 조정
 ladder = dict()
 snake = dict()
+cnt = 0
 
 for _ in range(ladder_num):
     x, y = map(int, input().split())
     ladder[x] = y
 
 for _ in range(snake_num):
-    u,v = map(int, input().split())
+    u, v = map(int, input().split())
     snake[u] = v
 
-queue = deque()
-queue.append(1) #1번칸부터 시작
-while len(queue)!=0:
-    queue.append()
+print(bfs(1, 100))
