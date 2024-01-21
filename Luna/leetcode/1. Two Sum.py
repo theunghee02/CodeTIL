e6c3b81194de0c96b7c9fa83fcbbe75a6aaa3e13
mjpark <80474535+parkminju20211126@users.noreply.github.com>
2024-01-21 21:01:27 +0900

    Two sum 2번째 풀이 (시간복잡도 줄이기)

--- Luna/leetcode/1. Two Sum.py
+++ Luna/leetcode/1. Two Sum.py
@@ -1,8 +1,20 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                temp = nums[i] + nums[j]
-                if temp == target:
-                    return [i,j]
-                    
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)-1):
+#             for j in range(i+1, len(nums)):
+#                 temp = nums[i] + nums[j]
+#                 if temp == target:
+#                     return [i,j]
+
+nums = [3,2,4]
+target = 6
+num_dict = {}
+
+for i, num in enumerate(nums):
+    num_dict[num] = i
+
+print(num_dict)
+for i , num in enumerate(nums):
+    res = target - num
+    if res in num_dict and num_dict[res] != i:
+        result = [i, num_dict[res]]
