e6651b368431589c8ea932f4f18e928dea2b1127
mjpark <80474535+parkminju20211126@users.noreply.github.com>
2024-01-29 22:13:49 +0900

    1/29 2 solved

--- Luna/leetcode/206.py
+++ Luna/leetcode/206.py
@@ -1,38 +1,21 @@
-from typing import List
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev, cur = None, head
+        while cur:
+            following = cur.next # 현재의 다음노드 저장
+            cur.next = prev # 방향 전환
+            prev = cur
+            cur = following
+        
+        new_head = prev
 
-class Solution:
-    def maxSubArrayRecursively(self, nums: List[int], start, end):
-        # base case
-        if end - start == 0:
-            return nums[end]
-
-        # recursive case
-        mid = (start + end) // 2
-        leftMax = self.maxSubArrayRecursively(nums, start, mid)
-        rightMax = self.maxSubArrayRecursively(nums, mid + 1, end)
-
-        # Combine the results
-        crossMax = self.maxCrossing(nums, start, mid, end)
-
-        # Return the maximum of three
-        return max(leftMax, rightMax, crossMax)
-
-    def maxSubArray(self, nums: List[int]) -> int:
-        print(self.maxSubArrayRecursively(nums, 0, len(nums) - 1))
-
-    def maxCrossing(self, nums, start, mid, end):
-        leftSum = float('-inf')
-        currSum = 0
-        for i in range(mid, start - 1, -1):
-            currSum += nums[i]
-            leftSum = max(leftSum, currSum)
-
-        rightSum = float('-inf')
-        currSum = 0
-        for i in range(mid + 1, end + 1):
-            currSum += nums[i]
-            rightSum = max(rightSum, currSum)
-        print(leftSum + rightSum)
-        return leftSum + rightSum
-
-nums = [5,4,-1,7,8]
+        return new_head
