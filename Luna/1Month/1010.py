9d115697c69ba481c87cbacf3525b1109b6aa7c6
mjpark <80474535+parkminju20211126@users.noreply.github.com>
2024-03-20 15:42:32 +0900

    3/20 다리 놓기

---
+++ Luna/1Month/1010.py
@@ -0,0 +1,24 @@
+# ## 풀이방법 정리
+# #1. 이전 sight에서 고른 오른쪽 sight를 m이라고 하자
+# #2. 오른쪽 sight에서 m+1 ~ 마지막까지 하나 고르고
+# #3. 나머지는 재귀함수로 넘김
+# ## 재귀함수는 내가 할일만 잘 정의하면 된다...!!
+#
+# # n =서 , m = 동
+def bridge_count(n, m, dp):
+    if dp[n][m] != -1:
+        return dp[n][m]
+    if n == m or m == 0:
+        dp[n][m] = 1
+    elif n == 1:
+        dp[n][m] = m
+    else:
+        dp[n][m] = bridge_count(n - 1, m - 1, dp) + bridge_count(n, m - 1, dp)
+
+    return dp[n][m]
+
+T = int(input())
+for _ in range(T):
+    a, b = map(int, input().split())
+    dp = [[-1 for _ in range(b + 1)] for _ in range(a + 1)]
+    print(bridge_count(a, b, dp))
---
+++ Luna/leetcode/88.py
@@ -0,0 +1,13 @@
+nums1 = [0]
+m = 0
+nums2 = [1]
+n = 1
+arr_list = []
+for i in nums1:
+    if i !=0:
+        arr_list.append(i)
+for j in nums2:
+    if j !=0:
+        arr_list.append(j)
+arr_list.sort()       
+print(arr_list)
