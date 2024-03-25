c83b4539f5095704e7bc9544ccfa32d8e489a226
mjpark <80474535+parkminju20211126@users.noreply.github.com>
2024-03-25 16:45:24 +0900

    수정 로봇청소기

--- Luna/1Month/14503.py
+++ Luna/1Month/14503.py
@@ -8,10 +8,12 @@ dc = [0,1,0,-1]
 visited = [[0]*M for _ in range(N)]
 visited[r][c] = 1
 cnt = 1 # 청소 횟수
+
 while True:
+    flag = False # 청소하지 않음
     ##  청소기가 바라보는 방향 - 왼쪽으로
     for i in range(4):
-        nd = (d+3)%4
+        d = (d+3)%4 # 반시계 방향으로 회전
         nr = r + dr[d]
         nc = c + dc[d]
         # 청소하지 않은 곳
@@ -21,12 +23,14 @@ while True:
                 r = nr
                 c = nc
                 cnt+=1
-    ## 4방향 모두 청소가 되어있다면
-        if 0 <= nr < N and 0 <= nc < M and matrix[nr][nc] == 1:
-            # 후진 가능 - 후진 후 청소
-            if matrix[r-dr[d]][c-dc[d]] == 0:
-                r,c = r-dr[d], c-dc[d]
-            # 후진 불가능 - stop
-            if matrix[r-dr[d]][c-dc[d]] == 1:
-                print(cnt)
+                flag = True # 청소 했음
                 break
+
+    ## 4방향 모두 청소가 되어있다면
+    if flag == False:
+        # 후진 가능한지 확인
+        if matrix[r-dr[d]][c-dc[d]] == 0: # 후진할 수 있다면
+            r, c = r-dr[d], c-dc[d]
+        else: # 후진 할 수 없다면
+            break
+print(cnt)
