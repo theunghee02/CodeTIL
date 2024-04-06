174c37be714f65e58b9e863d3b5a17de1e5dbd7e
mjpark <80474535+parkminju20211126@users.noreply.github.com>
2024-04-06 19:48:09 +0900

    시간 초과 문제 해결

--- Luna/1Month/14502.py
+++ Luna/1Month/14502.py
@@ -6,14 +6,21 @@ matrix = [list(map(int, input().split())) for _ in range(N)]
 dy = [1, -1, 0, 0]
 dx = [0, 0, -1, 1]
 from collections import deque
-def bfs():
+# for 문 반복을 줄이기 위해 바이러스 위치는 밖에서 구하기
+virus = []
+emptyCell = 0
+for i in range(N):
+    for j in range(M):
+        if matrix[i][j] == 2:
+            virus.append((i, j))
+        elif matrix[i][j] == 0:
+            emptyCell +=1
+def bfs(virus, emptyCell):
     visited = [[False for _ in range(M)]for _ in range(N)]
     queue = deque()
-
-    for i in range(N):
-        for j in range(M):
-            if matrix[i][j] == 2:
-                queue.append((i, j))
+    for v in virus: # 출발점 넣기
+        queue.append(v)
+    bfs_cnt = 0
 
     while queue:
         x, y = queue.popleft()
@@ -24,28 +31,26 @@ def bfs():
             if 0<=nx<N and 0<=ny<M:
                 if matrix[nx][ny] == 0 and visited[nx][ny] == False: # 빈칸이라면
                     visited[nx][ny] = True # 감염
-                    # bfs_cnt +=1
+                    bfs_cnt +=1 # 감염된 수
                     queue.append((nx, ny))
+    return emptyCell - bfs_cnt -3
 
-    bfs_cnt = 0
-    for i in range(N):
-        for j in range(M):
-            if matrix[i][j] == 0 and visited[i][j] == False:
-                bfs_cnt +=1
-    return bfs_cnt
-
-def makeWall(cnt = 0):
-    global max_safe_area
-    if cnt == 3:
-        max_safe_area = max(max_safe_area, bfs())
-        return
-
-    for i in range(N):
-        for j in range(M):
-            if matrix[i][j] == 0:
-                matrix[i][j] = 1 # 빈공간이라면 벽을 세우기
-                makeWall(cnt+1) # 다시 벽 만들러가
-                matrix[i][j] = 0
 max_safe_area = 0
-makeWall()
+
+for i in range(N):
+    for j in range(M):
+        if matrix[i][j] == 0:
+            matrix[i][j] = 1 # 빈공간이라면 벽을 세우기
+            for k in range(i, N):
+                for l in range(M):
+                    if matrix[k][l] == 0:
+                        matrix[k][l] = 1  # 빈공간이라면 벽을 세우기
+                        for m in range(k,N):
+                            for n in range(M):
+                                if matrix[m][n] == 0:
+                                    matrix[m][n] = 1  # 빈공간이라면 벽을 세우기
+                                    max_safe_area = max(max_safe_area, bfs(virus, emptyCell))
+                                    matrix[m][n] = 0
+                        matrix[k][l] = 0
+            matrix[i][j] = 0
 print(max_safe_area)
